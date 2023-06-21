2ca8637b3b20f88029a1711ec140a0de16c8ad66
Mariana Soares <81964220+Mariana-gs@users.noreply.github.com>
2023-06-21 16:33:38 -0300

    Creating Project

---
+++ FightingGame/Intermediate/Build/Win64/FightingGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30038
+#include "C:/Users/mariana.soares/Documents/GitHub/FightingGame/FightingGame/Intermediate/Build/Win64/FightingGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
